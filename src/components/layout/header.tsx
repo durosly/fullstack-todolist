11a4a1e897b888f68fc47986f1b576f85ff64094
durosly <slyboydon1@gmail.com>
2023-03-10 23:09:36 +0100

    add nav component

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Nav from "./nav";
 
 function Header() {
 	return (
@@ -28,68 +29,19 @@ function Header() {
 						tabIndex={0}
 						className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
 					>
-						<li>
-							<Link href="/features">Features</Link>
-						</li>
-						<li>
-							<Link href="/about-us">About Us</Link>
-						</li>
-						<li tabIndex={0}>
-							<a className="justify-between">
-								Theme
-								<svg
-									className="fill-current"
-									xmlns="http://www.w3.org/2000/svg"
-									width="24"
-									height="24"
-									viewBox="0 0 24 24"
-								>
-									<path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-								</svg>
-							</a>
-							<ul className="p-2">
-								<li>
-									<a>Submenu 1</a>
-								</li>
-								<li>
-									<a>Submenu 2</a>
-								</li>
-							</ul>
-						</li>
+						<Nav />
 					</ul>
 				</div>
-				<a className="btn btn-ghost normal-case text-xl">Tareasy</a>
+				<Link
+					href="/"
+					className="btn btn-ghost normal-case text-xl"
+				>
+					Tareasy
+				</Link>
 			</div>
 			<div className="navbar-center hidden lg:flex">
 				<ul className="menu menu-horizontal px-1">
-					<li>
-						<Link href="/features">Features</Link>
-					</li>
-					<li>
-						<Link href="/about-us">About Us</Link>
-					</li>
-					<li tabIndex={0}>
-						<a>
-							Theme
-							<svg
-								className="fill-current"
-								xmlns="http://www.w3.org/2000/svg"
-								width="20"
-								height="20"
-								viewBox="0 0 24 24"
-							>
-								<path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-							</svg>
-						</a>
-						<ul className="p-2">
-							<li>
-								<a>Submenu 1</a>
-							</li>
-							<li>
-								<a>Submenu 2</a>
-							</li>
-						</ul>
-					</li>
+					<Nav isMobile />
 				</ul>
 			</div>
 			<div className="navbar-end">
---
+++ src/components/layout/nav.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+
+type NavType = {
+	isMobile?: boolean;
+};
+
+function Nav({ isMobile }: NavType) {
+	return (
+		<>
+			<li>
+				<Link href="/features">Features</Link>
+			</li>
+			<li>
+				<Link href="/about-us">About Us</Link>
+			</li>
+			<li tabIndex={0}>
+				<a className="justify-between">
+					Theme
+					<svg
+						className={`fill-current ${isMobile && "rotate-90"}`}
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+					>
+						<path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+					</svg>
+				</a>
+				<ul className="p-2">
+					<li>
+						<a>Submenu 1</a>
+					</li>
+					<li>
+						<a>Submenu 2</a>
+					</li>
+				</ul>
+			</li>
+		</>
+	);
+}
+
+export default Nav;
