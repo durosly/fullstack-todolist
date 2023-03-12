5a04aaa99877a915bada0021671f2c02f37a154c
durosly <slyboydon1@gmail.com>
2023-03-12 02:45:51 +0100

    add login and signup pages

---
+++ src/pages/login.tsx
@@ -0,0 +1,7 @@
+import Wrapper from "@/components/layout/wrapper";
+
+function LoginPage() {
+	return <Wrapper>Login page</Wrapper>;
+}
+
+export default LoginPage;
---
+++ src/pages/signin.tsx
@@ -0,0 +1,7 @@
+import Wrapper from "@/components/layout/wrapper";
+
+function SigninPage() {
+	return <Wrapper>Sigin page</Wrapper>;
+}
+
+export default SigninPage;
