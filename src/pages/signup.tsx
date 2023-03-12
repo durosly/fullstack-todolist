42553ead92bdf1b844f337ff4f3ad71ceab9e0f5
durosly <slyboydon1@gmail.com>
2023-03-12 03:11:30 +0100

    add signup page info

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,7 +1,58 @@
 import Wrapper from "@/components/layout/wrapper";
+import Github from "@/icons/github";
+import Google from "@/icons/google";
 
 function SignupPage() {
-	return <Wrapper>Sigup page</Wrapper>;
+	return (
+		<Wrapper>
+			<form
+				className="max-w-md bg-slate-100 p-10 rounded-2xl mx-auto my-20"
+				action="/signup"
+			>
+				<div className="space-y-5">
+					<div>
+						<h1 className="text-4xl">Signup</h1>
+						<p>Enter your details to get started</p>
+					</div>
+					<div>
+						<input
+							type="text"
+							placeholder="Username..."
+							className="input w-full "
+						/>
+					</div>
+					<div>
+						<input
+							type="email"
+							placeholder="Email..."
+							className="input w-full "
+						/>
+					</div>
+					<div>
+						<input
+							type="password"
+							placeholder="Password..."
+							className="input w-full "
+						/>
+					</div>
+					<div>
+						<button className="btn btn-primary btn-block">
+							Submit
+						</button>
+					</div>
+				</div>
+				<div className="divider">Or sign up with</div>
+				<div className="text-center space-x-5">
+					<button className="btn btn-outline">
+						<Github className="w-5 fill-current" />
+					</button>
+					<button className="btn btn-outline">
+						<Google className="w-5 fill-current" />
+					</button>
+				</div>
+			</form>
+		</Wrapper>
+	);
 }
 
 export default SignupPage;
