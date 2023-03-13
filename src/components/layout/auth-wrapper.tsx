3ee536c8ca3c5c417a2a30d762b58a700ac7df15
durosly <slyboydon1@gmail.com>
2023-03-13 04:09:06 +0100

    add padding to container wrapper

--- src/components/layout/auth-wrapper.tsx
+++ src/components/layout/auth-wrapper.tsx
@@ -5,7 +5,7 @@ import MobileFooter from "@/components/layout/mobile-footer";
 function AuthWrapper({ children }: { children: ReactNode }) {
 	return (
 		<>
-			<div className="container mx-auto">
+			<div className="container mx-auto px-5 sm:px-0">
 				<Header isAuth />
 				{children}
 			</div>
