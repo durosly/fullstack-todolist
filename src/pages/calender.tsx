a9447142f9e9d01951330595b4fad03357103971
durosly <slyboydon1@gmail.com>
2023-03-19 01:16:25 +0100

    improve responsiveness

--- src/pages/calender.tsx
+++ src/pages/calender.tsx
@@ -29,11 +29,12 @@ function Calender() {
 
 	return (
 		<AuthWrapper>
-			<div className="flex justify-center gap-10 max-w-sm mx-auto mt-10">
+			<div className="flex justify-center items-center gap-10 max-w-sm mx-auto mt-10">
 				<span className="cursor-pointer">
-					<ArrowRight className="w-5 rotate-180" />
+					<ArrowRight className="w-2 sm:w-5 rotate-180" />
 				</span>
 				<input
+					className="form-control flex-row"
 					type="month"
 					id="start"
 					name="start"
@@ -41,18 +42,32 @@ function Calender() {
 					value="2018-05"
 				/>
 				<span className="cursor-pointer">
-					<ArrowRight className="w-5 " />
+					<ArrowRight className="w-3 sm:w-5 " />
 				</span>
 			</div>
 			<div className="max-w-sm mx-auto mt-10 bg-slate-100 p-5 rounded-2xl">
 				<ul className="flex">
-					<li className="flex-1 text-center p-2">Sun</li>
-					<li className="flex-1 text-center p-2">Mon</li>
-					<li className="flex-1 text-center p-2">Tue</li>
-					<li className="flex-1 text-center p-2">Wed</li>
-					<li className="flex-1 text-center p-2">Thur</li>
-					<li className="flex-1 text-center p-2">Fri</li>
-					<li className="flex-1 text-center p-2">Sat</li>
+					<li className="flex-1 text-center p-2 text-xs sm:text-base">
+						Sun
+					</li>
+					<li className="flex-1 text-center p-2 text-xs sm:text-base">
+						Mon
+					</li>
+					<li className="flex-1 text-center p-2 text-xs sm:text-base">
+						Tue
+					</li>
+					<li className="flex-1 text-center p-2 text-xs sm:text-base">
+						Wed
+					</li>
+					<li className="flex-1 text-center p-2 text-xs sm:text-base">
+						Thur
+					</li>
+					<li className="flex-1 text-center p-2 text-xs sm:text-base">
+						Fri
+					</li>
+					<li className="flex-1 text-center p-2 text-xs sm:text-base">
+						Sat
+					</li>
 				</ul>
 				<ul>
 					{dates.map((w, j) => (
@@ -61,7 +76,7 @@ function Calender() {
 								{w.map((d, i) => (
 									<li
 										key={i}
-										className={`flex-1 flex justify-center items-center aspect-square p-2 ${
+										className={`flex-1 flex justify-center items-center aspect-square p-2 text-xs sm:text-base ${
 											d.type === "current"
 												? "font-bold cursor-pointer hover:bg-slate-400 hover:text-white rounded-2xl"
 												: "font-light text-slate-400 cursor-not-allowed"
