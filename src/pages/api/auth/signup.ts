178b4aaf61c428774c7476380d0002d02dd953ac
durosly <slyboydon1@gmail.com>
2023-03-20 22:52:08 +0100

    successfully create new user record

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -1,14 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { UserBase } from '@/lib/connection'
 import { UserSignupResponseDataType } from '@/types/shared/user'
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<UserSignupResponseDataType>
 ) {
   if(req.method === 'POST') {
     
+    const { email, username, password } = req.body
 
+    await UserBase.create(
+      {
+        "Email": email,
+        "Username": username,
+        "Password": password
+      }
+    )
     res.status(200).json({ message: 'John Doe', status: true })
   } else {
     res.status(400).json({ status: false, message: "Invalid request method"})
