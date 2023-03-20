2f1071b8fd6cf2e06cd76e444c0b1eb37a7c1140
durosly <slyboydon1@gmail.com>
2023-03-20 18:10:57 +0100

    add toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import "@/styles/globals.css";
+import { Toaster } from "react-hot-toast";
 import "react-datepicker/dist/react-datepicker.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Component {...pageProps} />
+			<Toaster />
+		</>
+	);
 }
